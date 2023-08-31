41be550b6619cbc927e3e8631fcfd46598a08dd8
Jelle Broekhuijsen <consultancy@jll.io>
2023-08-31 17:21:09 +0200

    fmt

--- configurations/Config_via_Policy.ps1
+++ configurations/Config_via_Policy.ps1
@@ -1,12 +1,12 @@
 Configuration Config_via_Policy
 {
-    Import-DscResource -Name 'Environment' -ModuleName 'PSDscResources'
-    Environment MachineConfigurationExample {
-      Name   = 'MC_ENV_POLICY'
-      Value  = 'This was set by jelle@jll.io using machine configuration via Azure Policy'
-      Ensure = 'Present'
-      Target = @('Process', 'Machine')
-    }   
+  Import-DscResource -Name 'Environment' -ModuleName 'PSDscResources'
+  Environment MachineConfigurationExample {
+    Name   = 'MC_ENV_POLICY'
+    Value  = 'This was set by jelle@jll.io using machine configuration via Azure Policy'
+    Ensure = 'Present'
+    Target = @('Process', 'Machine')
+  }   
 }
 
 Config_via_Policy
