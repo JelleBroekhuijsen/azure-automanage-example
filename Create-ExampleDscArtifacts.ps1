ab90d2700b7b7c81d6bb9fca427f0664a116e7c9
Jelle Broekhuijsen <consultancy@jll.io>
2023-09-22 18:07:51 +0200

    Add try/catch/finally to cleanup after failure

--- Create-ExampleDscArtifacts.ps1
+++ Create-ExampleDscArtifacts.ps1
@@ -2,28 +2,37 @@
 #Requires -Modules @{ ModuleName="GuestConfiguration"; ModuleVersion = "4.5.0" }
 #Requires -Modules @{ ModuleName="PSDesiredStateConfiguration"; ModuleVersion = "2.0.7" }
 
+try{
 #Generate mofs
 . .\configurations\Config_via_Configuration_Management.ps1
 . .\configurations\Config_via_Policy.ps1
 . .\configurations\Install_Package_via_Chocolatey.ps1
 
 #Rename mofs
-Rename-Item .\Config_via_Configuration_Management\localhost.mof -NewName "Config_via_Configuration_Management.mof"
-Rename-Item .\Config_via_Policy\localhost.mof -NewName "Config_via_Policy.mof"
-Rename-Item .\Install_Package_via_Chocolatey\localhost.mof -NewName "Install_Package_via_Chocolatey.mof"
+  Rename-Item .\Config_via_Configuration_Management\localhost.mof -NewName "Config_via_Configuration_Management.mof"
+  Rename-Item .\Config_via_Policy\localhost.mof -NewName "Config_via_Policy.mof"
+  Rename-Item .\Install_Package_via_Chocolatey\localhost.mof -NewName "Install_Package_via_Chocolatey.mof"
 
-#Package mofs
-New-GuestConfigurationPackage -Name "Config_via_Configuration_Management" -Configuration .\Config_via_Configuration_Management\Config_via_Configuration_Management.mof -Type AuditAndSet -Force $true
-New-GuestConfigurationPackage -Name "Config_via_Policy" -Configuration .\Config_via_Policy\Config_via_Policy.mof -Type AuditAndSet -Force $true
-New-GuestConfigurationPackage -Name "Install_Package_via_Chocolatey" -Configuration .\Install_Package_via_Chocolatey\Install_Package_via_Chocolatey.mof -Type AuditAndSet -Force $true
+  #Package mofs
+  New-GuestConfigurationPackage -Name "Config_via_Configuration_Management" -Configuration .\Config_via_Configuration_Management\Config_via_Configuration_Management.mof -Type AuditAndSet -Force $true
+  New-GuestConfigurationPackage -Name "Config_via_Policy" -Configuration .\Config_via_Policy\Config_via_Policy.mof -Type AuditAndSet -Force $true
+  New-GuestConfigurationPackage -Name "Install_Package_via_Chocolatey" -Configuration .\Install_Package_via_Chocolatey\Install_Package_via_Chocolatey.mof -Type AuditAndSet -Force $true
 
-# Remove module folder from Install_Package_via_Chocolatey.zip
-Expand-Archive -Path .\Install_Package_via_Chocolatey.zip -DestinationPath .\Install_Package_via_Chocolatey_Staging -Force
-Remove-Item -Path .\Install_Package_via_Chocolatey_Staging\Modules -Recurse -Force
-Compress-Archive -Path .\Install_Package_via_Chocolatey_Staging\* -DestinationPath .\Install_Package_via_Chocolatey.zip -Force
-Remove-Item -Path .\Install_Package_via_Chocolatey_Staging -Recurse -Force
+  # Remove module folder from Install_Package_via_Chocolatey.zip
+  Expand-Archive -Path .\Install_Package_via_Chocolatey.zip -DestinationPath .\Install_Package_via_Chocolatey_Staging -Force
+  Remove-Item -Path .\Install_Package_via_Chocolatey_Staging\Modules\PackageManagement -Recurse -Force
+  Compress-Archive -Path .\Install_Package_via_Chocolatey_Staging\* -DestinationPath .\Install_Package_via_Chocolatey.zip -Force
+  Remove-Item -Path .\Install_Package_via_Chocolatey_Staging -Recurse -Force
+}
 
-#Cleanup mofs
-Remove-Item .\Config_via_Configuration_Management -Recurse -Force
-Remove-Item .\Config_via_Policy -Recurse -Force
-Remove-Item .\Install_Package_via_Chocolatey -Recurse -Force
+catch{
+
+}
+
+finally{
+  #Cleanup mofs
+  Remove-Item .\Config_via_Configuration_Management -Recurse -Force
+  Remove-Item .\Config_via_Policy -Recurse -Force
+  Remove-Item .\Install_Package_via_Chocolatey -Recurse -Force
+}
+
